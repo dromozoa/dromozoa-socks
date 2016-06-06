6110c9b2a67345b5b56c67ec2d8b3e035916bc1a
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-06-07 08:32:21 +0900

    io_serviceを分離。

--- dromozoa/socks/async_service.lua
+++ dromozoa/socks/async_service.lua
@@ -17,100 +17,26 @@
 
 local uint32 = require "dromozoa.commons.uint32"
 local unix = require "dromozoa.unix"
+local io_service = require "dromozoa.socks.io_service"
 local timer_service = require "dromozoa.socks.timer_service"
-
-local function add_reader(self, reader)
-  local fd = unix.fd.get(reader.fd)
-  if self.writers[fd] == nil then
-    if not self.selector:add(fd, unix.SELECTOR_READ) then
-      return unix.get_last_error()
-    end
-  else
-    if not self.selector:mod(fd, unix.SELECTOR_READ_WRITE) then
-      return unix.get_last_error()
-    end
-  end
-  self.readers[fd] = reader
-  reader.status = true
-  return self
-end
-
-local function add_writer(self, writer)
-  local fd = unix.fd.get(writer.fd)
-  if self.readers[fd] == nil then
-    if not self.selector:add(fd, unix.SELECTOR_WRITE) then
-      return unix.get_last_error()
-    end
-  else
-    if not self.selector:mod(fd, unix.SELECTOR_READ_WRITE) then
-      return unix.get_last_error()
-    end
-  end
-  self.writers[fd] = writer
-  writer.status = true
-  return self
-end
-
-local function del_reader(self, reader)
-  local fd = unix.fd.get(reader.fd)
-  if self.writers[fd] == nil then
-    if not self.selector:del(fd) then
-      return unix.get_last_error()
-    end
-  else
-    if not self.selector:mod(fd, unix.SELECTOR_WRITE) then
-      return unix.get_last_error()
-    end
-  end
-  self.readers[fd] = nil
-  reader.status = nil
-  return self
-end
-
-local function del_writer(self, writer)
-  local fd = unix.fd.get(writer.fd)
-  if self.readers[fd] == nil then
-    if not self.selector:del(fd) then
-      return unix.get_last_error()
-    end
-  else
-    if not self.selector:mod(fd, unix.SELECTOR_READ) then
-      return unix.get_last_error()
-    end
-  end
-  self.writers[fd] = nil
-  writer.status = nil
-  return self
-end
 
 local class = {}
 
 function class.new()
   return {
-    selector = unix.selector();
-    selector_timeout = unix.timespec(0.02, unix.TIMESPEC_TYPE_DURATION);
+    io = io_service();
     timer = timer_service();
-    readers = {};
-    writers = {};
   }
 end
 
 function class:add(handler)
-  local event = handler.event
-  if event == "read" then
-    return add_reader(self, handler)
-  elseif event == "write" then
-    return add_writer(self, handler)
-  end
+  self.io:add(handler)
+  return self
 end
 
 function class:del(handler)
-  local event = handler.event
-  if event == "read" then
-    return del_reader(self, handler)
-  elseif event == "write" then
-    return del_writer(self, handler)
-  end
+  self.io:del(handler)
+  return self
 end
 
 function class:start()
@@ -141,31 +67,12 @@ function class:dispatch(thread)
     if self.stopped then
       return self
     end
-    local result = self.selector:select(self.selector_timeout)
+    local result, message = self.io:dispatch()
     if not result then
-      if unix.get_last_errno() ~= unix.EINTR then
-        return unix.get_last_error()
-      end
-    else
-      self.timer:update()
-      for i = 1, result do
-        local fd, event = self.selector:event(i)
-        if uint32.band(event, unix.SELECTOR_READ) ~= 0 then
-          local result, message = self.readers[fd]:dispatch(self, "read")
-          if not result then
-            return nil, message
-          end
-        end
-        if uint32.band(event, unix.SELECTOR_WRITE) ~= 0 then
-          local result, message = self.writers[fd]:dispatch(self, "write")
-          if not result then
-            return nil, message
-          end
-        end
-      end
-      if self.stopped then
-        return self
-      end
+      return nil, message
+    end
+    if self.stopped then
+      return self
     end
   end
 end
