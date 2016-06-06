066031361645522ca1307f53d77b4f6c40a54b8d
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-06-06 11:00:26 +0900

    ファイル名を変更。

--- test/test_async_when.lua
+++ test/test_async_when.lua
@@ -15,7 +15,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-socks.  If not, see <http://www.gnu.org/licenses/>.
 
-local async_any_state = require "dromozoa.socks.async_any_state"
+local async_when_state = require "dromozoa.socks.async_when_state"
 local async_future = require "dromozoa.socks.async_future"
 local async_deferred_state = require "dromozoa.socks.async_deferred_state"
 local async_service = require "dromozoa.socks.async_service"
@@ -38,17 +38,17 @@ assert(service:dispatch(coroutine.create(function ()
   assert(not f2:is_ready())
   assert(not f3:is_ready())
 
-  async_future(async_any_state(service, f1, f2, f3)):wait()
+  async_future(async_when_state(service, "any", f1, f2, f3)):wait()
   assert(not f1:is_ready())
   assert(f2:is_ready())
   assert(not f3:is_ready())
 
-  async_future(async_any_state(service, f1, f3)):wait()
+  async_future(async_when_state(service, "any", f1, f3)):wait()
   assert(not f1:is_ready())
   assert(f2:is_ready())
   assert(f3:is_ready())
 
-  async_future(async_any_state(service, f1)):wait_for(0.2)
+  async_future(async_when_state(service, "any", f1)):wait_for(0.2)
   assert(not f1:is_ready())
   assert(f2:is_ready())
   assert(f3:is_ready())
