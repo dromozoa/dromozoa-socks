aae3a827fab8605a0f0f87707b90c7fa3814b007
Tomoyuki Fujimori <moyu@dromozoa.com>
2016-07-19 13:59:14 +0900

    リファクタリング。

--- dromozoa/socks/crypt_apache_md5.lua
+++ dromozoa/socks/crypt_apache_md5.lua
@@ -20,6 +20,30 @@ local sequence_writer = require "dromozoa.commons.sequence_writer"
 local uint32 = require "dromozoa.commons.uint32"
 local crypt_base64 = require "dromozoa.socks.crypt_base64"
 
+local function loop(rounds, A_C, P, S)
+  for i = 1, rounds do
+    local C = md5()
+    if i % 2 == 0 then
+      C:update(P)
+    else
+      C:update(A_C)
+    end
+    if i % 3 ~= 1 then
+      C:update(S)
+    end
+    if i % 7 ~= 1 then
+      C:update(P)
+    end
+    if i % 2 == 0 then
+      C:update(A_C)
+    else
+      C:update(P)
+    end
+    A_C = C:finalize("bin")
+  end
+  return A_C
+end
+
 return function (key, salt)
   local salt_string = salt:match("^%$apr1%$([^%$]+)")
   if salt_string then
@@ -28,24 +52,22 @@ return function (key, salt)
     error("unsupported salt")
   end
 
-  local A = md5()
-  A:update(key)
-  A:update("$apr1$")
-  A:update(salt_string)
-
   local B = md5()
   B:update(key)
   B:update(salt_string)
   B:update(key)
   local B = B:finalize("bin")
 
+  local A = md5()
+  A:update(key)
+  A:update("$apr1$")
+  A:update(salt_string)
   local n = #key
   while n > 16 do
     n = n - 16
     A:update(B)
   end
   A:update(B:sub(1, n))
-
   local n = #key
   while n > 0 do
     if uint32.band(n, 1) == 1 then
@@ -57,34 +79,15 @@ return function (key, salt)
   end
   local A = A:finalize("bin")
 
-  for i = 0, 999 do
-    local C = md5()
-    if i % 2 == 1 then
-      C:update(key)
-    else
-      C:update(A)
-    end
-    if i % 3 ~= 0 then
-      C:update(salt_string)
-    end
-    if i % 7 ~= 0 then
-      C:update(key)
-    end
-    if i % 2 == 1 then
-      C:update(A)
-    else
-      C:update(key)
-    end
-    A = C:finalize("bin")
-  end
+  local C = loop(1000, A, key, salt_string)
 
   local out = sequence_writer()
   out:write("$apr1$", salt_string, "$")
-  crypt_base64.encode(out, A, 1, 7, 13)
-  crypt_base64.encode(out, A, 2, 8, 14)
-  crypt_base64.encode(out, A, 3, 9, 15)
-  crypt_base64.encode(out, A, 4, 10, 16)
-  crypt_base64.encode(out, A, 5, 11, 6)
-  crypt_base64.encode(out, A, 12)
+  crypt_base64.encode(out, C, 1, 7, 13)
+  crypt_base64.encode(out, C, 2, 8, 14)
+  crypt_base64.encode(out, C, 3, 9, 15)
+  crypt_base64.encode(out, C, 4, 10, 16)
+  crypt_base64.encode(out, C, 5, 11, 6)
+  crypt_base64.encode(out, C, 12)
   return out:concat()
 end
